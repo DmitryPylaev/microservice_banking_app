67c0bca8a22cf7ebd09a8a6d279cc63f722fab89
Дмитрий Пылаев <pilaevdmitry@yandex.ru>
2023-07-25 09:05:51 +0400

    Entrypoint

--- conveyor/src/main/java/Main.java
+++ conveyor/src/main/java/Main.java
@@ -1,5 +1,12 @@
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.neoflex.java2023")
 public class Main {
     public static void main(String[] args) {
-
+        ApplicationContext context = new SpringApplicationBuilder(Main.class).headless(false).run(args);
     }
 }
