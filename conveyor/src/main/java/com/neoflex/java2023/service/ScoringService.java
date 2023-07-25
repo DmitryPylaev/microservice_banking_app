4c159380de884b13b85ec406599ab010d4e9bde7
Дмитрий Пылаев <pilaevdmitry@yandex.ru>
2023-07-25 14:40:52 +0400

    ScoringService + test

--- conveyor/src/main/java/com/neoflex/java2023/service/ScoringService.java
+++ conveyor/src/main/java/com/neoflex/java2023/service/ScoringService.java
@@ -1,23 +1,35 @@
 package com.neoflex.java2023.service;
 
-import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
 
 @Service
-@AllArgsConstructor
 public class ScoringService {
 
+    private final Double baseRate;
+
+    public ScoringService(@Value("${baseRate}") Double baseRate) {
+        this.baseRate = baseRate;
+    }
+
     public BigDecimal evaluateTotalAmountServices(BigDecimal amount, Boolean isInsuranceEnabled) {
-        return BigDecimal.valueOf(1.05);
+        return (isInsuranceEnabled)?amount:BigDecimal.valueOf(amount.intValue()/2);
     }
 
     public BigDecimal calculateRate(Boolean isInsuranceEnabled, Boolean isSalaryClient) {
-        return BigDecimal.valueOf(2.0);
+        BigDecimal result = BigDecimal.valueOf(baseRate);
+        if (isInsuranceEnabled) result = result.subtract(BigDecimal.ONE);
+        if (isSalaryClient) result = result.subtract(BigDecimal.valueOf(0.5));
+        return result;
     }
 
     public BigDecimal calculateMonthlyPayment(BigDecimal amount, Integer term, BigDecimal rate) {
-        return BigDecimal.valueOf(3.0);
+        BigDecimal monthRate = rate.divide(BigDecimal.valueOf(12 * 100), new MathContext(7));
+        BigDecimal percent = monthRate.add(BigDecimal.ONE).pow(term);
+        BigDecimal annuityCoefficient = monthRate.multiply(percent.divide(percent.subtract(BigDecimal.ONE), new MathContext(7)));
+        return amount.multiply(annuityCoefficient, new MathContext(7));
     }
 }
---
+++ conveyor/src/test/java/com/neoflex/java2023/service/ScoringServiceTest.java
@@ -0,0 +1,55 @@
+package com.neoflex.java2023.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+
+@SpringBootTest(classes = {ScoringService.class})
+@ComponentScan("com.neoflex.java2023")
+class ScoringServiceTest {
+    private final Double baseRate;
+    private final ScoringService service;
+
+    @Autowired
+    public ScoringServiceTest(@Value("${baseRate}") Double baseRate, ScoringService service) {
+        this.baseRate = baseRate;
+        this.service = service;
+    }
+
+
+    @Test
+    void evaluateTotalAmountServices() {
+        BigDecimal amount = BigDecimal.valueOf(100);
+
+        Assertions.assertEquals(amount, service.evaluateTotalAmountServices(amount, true));
+        Assertions.assertEquals(amount.divide(BigDecimal.valueOf(2), new MathContext(7)), service.evaluateTotalAmountServices(amount, false));
+    }
+
+    @Test
+    void calculateRate() {
+        Assertions.assertEquals(BigDecimal.valueOf(baseRate).subtract(BigDecimal.valueOf(1.5)),
+                service.calculateRate(true, true));
+
+        Assertions.assertEquals(BigDecimal.valueOf(baseRate).subtract(BigDecimal.valueOf(1)),
+                service.calculateRate(true, false));
+
+        Assertions.assertEquals(BigDecimal.valueOf(baseRate).subtract(BigDecimal.valueOf(0.5)),
+                service.calculateRate(false, true));
+
+        Assertions.assertEquals(BigDecimal.valueOf(baseRate),
+                service.calculateRate(false, false));
+    }
+
+    @Test
+    void calculateMonthlyPayment() {
+        BigDecimal expected = BigDecimal.valueOf(18714.44);
+        BigDecimal result = service.calculateMonthlyPayment(BigDecimal.valueOf(300000), 18, BigDecimal.valueOf(15));
+        Assertions.assertTrue(Math.abs(expected.doubleValue() - result.doubleValue()) < 2.0);
+    }
+}
